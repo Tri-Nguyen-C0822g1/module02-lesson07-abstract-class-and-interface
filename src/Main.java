1c81b6128b27b19bdb9a9bf6fc9a1dfb426a1ad0
Tri-Nguyen-C0822g1 <tringuyen10396@gmail.com>
2022-10-14 23:57:29 +0700

    add project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+//    public static void main(String[] args) {
+//        Chicken chicken = new Chicken();
+//        System.out.println(chicken.makeSound());
+//        Tiger tiger = new Tiger();
+//        System.out.println(tiger.makeSound());
+//    }
+}
